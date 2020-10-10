454bbac3f276ae968fa3e4f649f6bad6194d0a93
Evgeniy Kulikov <evgenii49-9@yndex.ru>
2020-10-10 22:52:28 +0300

    второй урок

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-var money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission,
-    period;
- alert( 'Первый урок. Переменные');
- console.log('Все работает');
+var money = 50000, 
+    income = 'фриланс', 
+    addExpenses =' интернет, такси, коммуналка ', 
+    deposit = true, 
+    mission = 150000,
+    period  = 5;
+// 1 ответ 
+ console.log(money);
+ console.log(income);
+ console.log(deposit);
+// 2 ответ
+console.log(addExpenses.length);
+// 3 ответ 
+console.log('Период равен'+' '+period+' '+ 'месяцев.'+' '
+            +'Цель заработать'+' '+ mission+' '+ 'рублей/долларов/гривен/юани');
+//  4 ответ
+ console.log(addExpenses.toLowerCase().split(','));    
+// 5 ответ
