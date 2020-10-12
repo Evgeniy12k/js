700bddc4f02012bf3b20bb12dca9860ac236e5f0
Evgeniy Kulikov <evgenii49-9@yndex.ru>
2020-10-12 17:33:12 +0300

    Работа с переменными

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
-var money = 50000, 
-    income = 'фриланс', 
-    addExpenses =' интернет, такси, коммуналка ', 
-    deposit = true, 
-    mission = 150000,
-    period  = 5,
-    budgetDay;
-// 1 ответ 
- console.log(money);
- console.log(income);
- console.log(deposit);
-// 2 ответ
-console.log(addExpenses.length);
-// 3 ответ 
-console.log('Период равен'+' '+period+' '+ 'месяцев.'+' '
-            +'Цель заработать'+' '+ mission+' '+ 'рублей/долларов/гривен/юани');
-//  4 ответ
- console.log(addExpenses.toLowerCase().split(','));    
-// 5 ответ
-console.log(budgetDay=money/30);
+'use strickt';
+// let money,
+//     income = 'фриланс', 
+//     addExpenses, 
+//     deposit = true, 
+//     mission = 150000,
+//     period  = 5,
+//     budgetDay;
+// // 1 ответ 
+ 
+//  console.log(income);
+//  console.log(deposit);
+// // 2 ответ
+// // 3 ответ 
+// console.log('Период равен'+' '+period+' '+ 'месяцев.'+' '
+//             +'Цель заработать'+' '+ mission+' '+ 'рублей/долларов/гривен/юани');
+  
+// // 5 ответ
+// console.log(budgetDay=money/30);
+
+// 1 вопрос
+// let money = confirm('Ваш месячный доход?');
+//   console.log(money);
+// // 2 вопрос
+// let addExpenses= prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+// console.log(addExpenses);
+// // 3 вопрос
+
+ let deposit= confirm('Есть ли у вас депозит в банке?');
+console.log(deposit);
+
+ let expenses1= prompt('Введите обязательную статью расходов?');
+ console.log(expenses1);
+
+ let amount1= prompt('Во сколько это обойдется?');
+ console.log(amount1);
+
+ let amount2= prompt('Во сколько это обойдется?');
+ console.log(amount2);
