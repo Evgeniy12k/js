93c393068057e12a2668af1d5774cbed058ad801
Evgeniy Kulikov <evgenii49-9@yndex.ru>
2020-10-15 00:45:25 +0300

    эциклыэ

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 'use strickt';
 
-let money = 50000, 
-    income = 'фриланс', 
-    addExpenses =' интернет, такси, коммуналка ', 
+ let isNumber = function(n){
+    return !isNaN(parseFloat(n)) && isFinite(n);
+ };
+
+let money, 
+    income = 'фриланс',  
     deposit = true, 
     mission = 150000,
     period  = 5;
@@ -13,64 +16,123 @@ let money = 50000,
 // ================================
 
 // 1 вопрос
-let expenses1 = prompt('Введите обязательную статью расходов?');
-let amount1 = +prompt('Во сколько это обойдется?');
+// let expenses1 = prompt('Введите обязательную статью расходов?');
+// let amount1 = +prompt('Во сколько это обойдется?');
 
-let expenses2 = prompt('Введите обязательную статью расходов?' );
-let amount2 = prompt('Во сколько это обойдется?');
+// let expenses2 = prompt('Введите обязательную статью расходов?','квартира' );
+// let amount2 = +prompt('Во сколько это обойдется?');
 
 
-function getExpensesMonth(amount1, amount2 ){
-   console.log(arguments);
-   return amount1 + amount2;
-}
- let expenses =  getExpensesMonth(+amount1,+amount2 );
-console.log('обязательные расходы '+ expenses);
+// ================================
+// Урок 5.Циклы.
+// ================================
+  let start = function(){
+   do{
+      money = prompt('Ваш месячный доход?');
+   }
+  
 
- // 2 вопро
- // let money = prompt('Ваш месячный доход?')
- function getAccumulatedMonth(expenses1, expenses2 ){
-    return money - expenses;
-  }
-   let accumulatedMonth = getAccumulatedMonth(+money - +expenses );
-  console.log( 'Накопления за месяц '+accumulatedMonth);
- // 3 вопрос
- // выполнил выше
- // 4 вопрос
- // let mission = prompt('цель')
- function getTargetMonth (expenses1, expenses2 ){
-    return  Math.ceil(mission / accumulatedMonth);
-  }
- let TargetMonth = getTargetMonth(+mission / +accumulatedMonth );
-  console.log('достигнуть цели через '+ TargetMonth)
- //  6 вопро
- // 7 вопрос
- function getbudgetDay(TargetMonth){
-    return Math.floor(+accumulatedMonth/30);
+   while ( !isNumber(money)){
+      money = prompt('Ваш месячный доход?');
+
+   }
   };
- let budgetDay = getbudgetDay();
-    console.log('Бюджет дня '+  +budgetDay)
- let showTypeOf = function(data){
-    console.log(data)
-    };
-    showTypeOf(getExpensesMonth);
-    showTypeOf( getTargetMonth);
-    showTypeOf(budgetDay)
- let getStatusIncome =function(){
-       if (budgetDay > 1200) {
-          return('высокий доход');
-         } else if (budgetDay>600 && budgetDay>1200){       
-          return('средний доход');
-         } else if(budgetDay < 600 && budgetDay>0 ){       
-          return('средний доход');
-         } else if (budgetDay === 1200){      
-          return('высокий доход'); 
-         } else if (budgetDay === 600){       
-          return('средний доход'); 
-         } else if (budgetDay === 0){      
-          return('низкий доход'); 
-         } else {      
-          return('что-то пошло не так'); 
-         }
-       }; 
-     console.log(getStatusIncome());
+  start();
+
+
+let showTypeOf = function(item){
+      console.log(item);
+      };
+      showTypeOf( money);
+      showTypeOf( income);
+      showTypeOf(deposit);
+
+let expenses1, expenses2;
+
+let addExpenses= prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+   console.log(addExpenses.toLowerCase().split(','));
+function getExpensesMonth(){
+   let sum  = 0;
+   let sum2;
+
+    for (let i = 0; i<2; i++){
+       
+      if (i===0){
+          expenses1 = prompt('Введите обязательную статью расходов?');
+      }if(i===1){
+          expenses2 = prompt('Введите обязательную статью расходов?','квартира' );
+      };
+       do{
+         sum2 = prompt('Во сколько это обойдется?');
+       }
+       while ( !isNumber(sum2)){
+         sum2 = prompt('Во сколько это обойдется?')
+      }
+      sum += Number(sum2);
+      // if (!isNaN (sum2)){
+      //    console.log(typeof (sum2));
+      // }
+    }
+    console.log(sum2);
+   return sum;
+};
+ let expensesAmount = getExpensesMonth();
+console.log('обязательные расходы '+ expensesAmount);
+
+//  // 2 вопро
+//  // let money = prompt('Ваш месячный доход?')
+//  function getAccumulatedMonth(expenses1, expenses2 ){
+//     return money - expenses;
+//   }
+//    let accumulatedMonth = getAccumulatedMonth(+money - +expenses );
+//   console.log( 'Накопления за месяц '+accumulatedMonth);
+//  // 3 вопрос
+//  // выполнил выше
+//  // 4 вопрос
+//  // let mission = prompt('цель')
+  
+// function getTargetMonth (expenses1, expenses2 ){
+//      return  Math.ceil(mission / accumulatedMonth);
+
+     
+//    }
+//   let TargetMonth = getTargetMonth(+mission / +accumulatedMonth );
+//   if (TargetMonth < 0 ){
+//    console.log('Цель не будет достигнута ');
+//   } else {
+//    console.log('Цель  будет достигнута '+ TargetMonth);
+//   }
+  
+ 
+
+//  //  6 вопро
+//  // 7 вопрос
+//  function getbudgetDay(TargetMonth){
+//     return Math.floor(+accumulatedMonth/30);
+//   };
+//  let budgetDay = getbudgetDay();
+//     console.log('Бюджет дня '+  +budgetDay)
+//  let showTypeOf = function(data){
+//     console.log(data)
+//     };
+//     showTypeOf(getExpensesMonth);
+//     showTypeOf( getTargetMonth);
+//     showTypeOf(budgetDay)
+//  let getStatusIncome =function(){
+//        if (budgetDay > 1200) {
+//           return('высокий доход');
+//          } else if (budgetDay>600 && budgetDay>1200){       
+//           return('средний доход');
+//          } else if(budgetDay < 600 && budgetDay>0 ){       
+//           return('средний доход');
+//          } else if (budgetDay === 1200){      
+//           return('высокий доход'); 
+//          } else if (budgetDay === 600){       
+//           return('средний доход'); 
+//          } else if (budgetDay === 0){      
+//           return('низкий доход'); 
+//          } else {      
+//           return('что-то пошло не так'); 
+//          }
+//        }; 
+//      console.log(getStatusIncome());
