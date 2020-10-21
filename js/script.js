0334797c297df5e6c6d9427897321e39197c4b89
Evgeniy Kulikov <evgenii49-9@yndex.ru>
2020-10-22 00:16:16 +0300

    DOM, доступ к объектам

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+// кнопка Рассчитать
+let Calculate = document.getElementById('start');
+console.log(Calculate);
+
+ // Кнопки +
+ let buttonPlusOne = document.getElementsByTagName('button')[0];
+ console.log(buttonPlusOne);
+
+ let buttonPlusTo = document.getElementsByTagName('button')[1];
+ console.log(buttonPlusTo);
+
+
+// checkbox
+let checkbox = document.querySelectorAll('.deposit-checkmark')[0];
+console.log(checkbox);
+
+// возможных доходов
+
+ let possibleIncomeOne = document.querySelectorAll('.additional_income-item')[0];
+ console.log(possibleIncomeOne);
+
+ let possibleIncomeTo = document.querySelectorAll('.additional_income-item')[1];
+ console.log(possibleIncomeTo);
