031e549ecbf787543a3aa8c35684ae39b845da33
Evgeniy Kulikov <evgenii49-9@yndex.ru>
2020-10-23 16:04:14 +0300

    добавил все строки

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ let Calculate = document.getElementById('start');
 console.log(Calculate);
 
  // Кнопки +
- let buttonPlusOne = document.getElementsByTagName('button')[0];
- console.log(buttonPlusOne);
+ let button1 = document.getElementsByTagName('button')[0];
+ console.log(button1);
 
- let buttonPlusTo = document.getElementsByTagName('button')[1];
- console.log(buttonPlusTo);
+ let button2 = document.getElementsByTagName('button')[1];
+ console.log(button2);
 
 
 // checkbox
@@ -17,44 +17,80 @@ console.log(checkbox);
 
 // возможных доходов
 
- let possibleIncomeOne = document.querySelectorAll('.additional_income-item')[0];
- console.log(possibleIncomeOne);
+ let possible1 = document.querySelectorAll('.additional_income-item')[0];
+ console.log(possible1);
 
- let possibleIncomeTo = document.querySelectorAll('.additional_income-item')[1];
- console.log(possibleIncomeTo);
+ let possible2 = document.querySelectorAll('.additional_income-item')[1];
+ console.log(possible2);
 
 // бюджет
-let budgetDayValue = document.getElementsByClassName('budget_day-value')[0];
-console.log(budgetDayValue);
+let budgetdayvalue = document.getElementsByClassName('budget_day-value')[0];
+console.log(budgetdayvalue);
 
-let expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0];
-console.log( expensesMonthValue);
+let expensesmonthvalue = document.getElementsByClassName('expenses_month-value')[0];
+console.log( expensesmonthvalue);
 
-let additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0];
-console.log( additionalIncomeValue);
+let additional1 = document.getElementsByClassName('additional_income-value')[0];
+console.log( additional1);
 
-let additionalExpensesValue = document.getElementsByClassName('additional_expenses-value')[0];
-console.log( additionalExpensesValue);
+let additional2 = document.getElementsByClassName('additional_expenses-value')[0];
+console.log( additional2);
 
-let incomePeriodValue = document.getElementsByClassName('income_period-value')[0];
-console.log( incomePeriodValue);
+let income = document.getElementsByClassName('income_period-value')[0];
+console.log( income);
 
 
-let targetMonthValue = document.getElementsByClassName('expenses_month-value')[0];
-console.log( targetMonthValue);
+let target = document.getElementsByClassName('expenses_month-value')[0];
+console.log( target);
 
 // поля ввода слева
+// первое название
+let input1 = document.querySelector('[class="income-title"]');
+console.log( input1);
 
-let inputOne = document.querySelector('[class="income-title"]');
-console.log( inputOne);
+// строка сумма
+let sum1 = document.querySelector('[class="income-amount"]');
+console.log( sum1);
 
-let inputTo = document.querySelector('[class="income-title"]');
-console.log( inputTo);
+// второе  название
+let input2 = document.querySelector('[class="income-title"]');
+console.log( input2);
 
-let inputThird = document.querySelector('[class="income-title"]');
-console.log( inputThird);
+// строка сумма 2
+
+let sum2 = document.querySelector('[class="additional_income-item"]');
+console.log( sum2);
+
+
+// третье  название
+let input3 = document.querySelector('[class="income-title"]');
+console.log( input3);
+
+// строка сумма 3
+let sum3 = document.querySelector('[class="additional_income-item"]');
+console.log( sum3);
 
 //range 
 let range = document.querySelector('[type="range"]');
 console.log( range);
+
+// сумма 
+let summa = document.querySelector('[class="target-amount"]');
+console.log( summa);
+
+// месячный доход сумма
+
+let sum = document.querySelector('[class="salary-amount"]');
+console.log( sum);
+
+
+// обязательные расходы
+
+// название
+let input4 = document.querySelector('[class="expenses-title"]');
+console.log( input4);
+
+// сумма
+let sum4 = document.querySelector('[class="expenses-amount"]');
+console.log( sum4);
 
