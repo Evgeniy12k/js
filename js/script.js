1c27b33ba4ca447efed1d4a4df83cc2f060f2307
Evgeniy Kulikov <evgenii49-9@yndex.ru>
2020-10-23 21:20:54 +0300

    измения в qwerySelector

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ let Calculate = document.getElementById('start');
 console.log(Calculate);
 
  // Кнопки +
- let buttonPlusOne = document.getElementsByTagName('button')[0];
- console.log(buttonPlusOne);
+ let but = document.getElementsByTagName('button')[0];
+ console.log(but);
 
- let buttonPlusTo = document.getElementsByTagName('button')[1];
- console.log(buttonPlusTo);
+ let button = document.getElementsByTagName('button')[1];
+ console.log(button);
 
 
 // checkbox
@@ -17,8 +17,78 @@ console.log(checkbox);
 
 // возможных доходов
 
- let possibleIncomeOne = document.querySelectorAll('.additional_income-item')[0];
- console.log(possibleIncomeOne);
+ let possib = document.querySelectorAll('.additional_income-item')[0];
+ console.log(possib);
 
- let possibleIncomeTo = document.querySelectorAll('.additional_income-item')[1];
- console.log(possibleIncomeTo);
+ let possible = document.querySelectorAll('.additional_income-item')[1];
+ console.log(possible);
+
+// бюджет
+let budgetdayvalue = document.getElementsByClassName('budget_day-value')[0];
+console.log(budgetdayvalue);
+
+let expensesmonthvalue = document.getElementsByClassName('expenses_month-value')[0];
+console.log( expensesmonthvalue);
+
+let addition = document.getElementsByClassName('additional_income-value')[0];
+console.log( addition);
+
+let additional = document.getElementsByClassName('additional_expenses-value')[0];
+console.log( additional);
+
+let income = document.getElementsByClassName('income_period-value')[0];
+console.log( income);
+
+
+let target = document.getElementsByClassName('expenses_month-value')[0];
+console.log( target);
+
+// поля ввода слева
+// первое название
+let ferst = document.querySelector('[class="income-title"]');
+console.log( ferst);
+
+// строка сумма
+let sferst = document.querySelector('.income-amount');
+console.log( sferst);
+
+ // второе  название
+ let second = document.querySelector('input.income-title');
+ console.log( second);
+
+ // строка сумма 2
+
+ let ssecond = document.querySelector('.additional_income-item');
+ console.log( ssecond);
+
+
+ // третье  название
+ let third = document.querySelector('input.income-title');
+ console.log( third);
+
+ // строка сумма 3
+ let sthird = document.querySelector('input.additional_income-item');
+ console.log( sthird);
+
+ //range 
+ let range = document.querySelector('.period-select');
+ console.log( range);
+
+ // сумма 
+ let summa = document.querySelector('input.target-amount');
+ console.log( summa);
+
+ // месячный доход сумма
+
+ let total = document.querySelector('input.salary-amount');
+ console.log( total);
+
+
+ // обязательные расходы
+ // название
+ let fourth = document.querySelector('input.expenses-title');
+ console.log(fourth);
+
+ // сумма
+ let cost = document.querySelector('input.expenses-amount');
+ console.log(cost);
