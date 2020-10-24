012415b0e0d90d378fb9e867b44cec53cfb97aed
Evgeniy Kulikov <evgenii49-9@yndex.ru>
2020-10-24 16:47:22 +0300

    Добавил все пункты по заданию

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 'use strict';
+
+// смена картинки
+ 
 document.body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
- // удаление рекламы
+ 
+// удаление рекламы
  const spam = document.querySelector('.adv');
  console.log(spam);
 
  spam.remove();
 
- // заголовок в кние 3
+// ========================
+// заголовок в книге 3
+// ========================
   const titleBook = document.querySelectorAll('h2');
  console.log(titleBook);
  titleBook[4].textContent = 'Книга 3. this и Прототипы Объектов';
 
-
- // смена картинки
- // const image = document.getElementById('background');
-
- // меняем книги местами
+//  =======================
+//   меняем книги местами
+//  =======================
 const booksParent = document.querySelector('.books');
  const books = document.querySelectorAll('.book');
  console.log(books);
@@ -42,3 +46,51 @@ const booksParent = document.querySelector('.books');
   booksParent.append(book4);
   booksParent.append(book5);
   booksParent.append(book6);
+
+  // ==============================
+  // 6 глава добавляю новый элемент
+  // ===============================
+
+   
+  const bookUl = document.querySelectorAll('ul');
+   console.log(bookUl);
+
+   const h = bookUl[5];
+   console.log(h);
+
+   const list = h.querySelectorAll('li');
+   console.log(list);
+   
+
+  // создаю новый элемент
+  const newElem = document.createElement('li');
+  newElem.textContent = 'Глава 8: За пределами ES6';
+
+  h.append(newElem);
+
+  list[9].before(newElem);
+
+
+  // ==============================
+  // Меняю местами элементы в главе 2
+  // ===============================
+
+  const secondBook =bookUl[1];
+  const list2 = secondBook.querySelectorAll('li');
+  console.log(list2);
+  list2[4].before(list2[6]);
+  list2[4].before(list2[8]);
+  list2[9].after(list2[2]);
+
+  // ==============================
+  // Меняю местами элементы в главе 5
+  // ===============================
+
+  const bookFive =bookUl[4];
+  const list3 = bookFive.querySelectorAll('li');
+  console.log(list3);
+  list3[2].before(list3[9]);
+  list3[2].before(list3[3]);
+
+  list3[8].before(list3[5]);
+  list3[2].before(list3[4]);
