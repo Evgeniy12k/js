82aad710a1217b330f0e1d9ec1b0e5046780fd19
Evgeniy Kulikov <evgenii49-9@yndex.ru>
2020-11-06 00:11:36 +0300

    Timer

--- js/script.js
+++ js/script.js
@@ -1,94 +1,53 @@
+window.addEventListener('DOMContentLoaded', function(){
 'use strict';
-// кнопка Рассчитать
-let Calculate = document.getElementById('start');
-console.log(Calculate);
 
- // Кнопки +
- let but = document.getElementsByTagName('button')[0];
- console.log(but);
+// timer
+function countTimer(deadline){
 
- let button = document.getElementsByTagName('button')[1];
- console.log(button);
+    let timerHours = document.querySelector('#timer-hours'),
+        timerMinutes = document.querySelector('#timer-minutes'),
+        timerSeconds = document.querySelector('#timer-seconds');
+        
+//    вычисление даты: часы, минуты, секунды
+        function getTimeRemaining(){
+        let dateStop = new Date(deadline).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            seconds = Math.floor(timeRemaining % 60),
+            minutes = Math.floor((timeRemaining / 60) % 60 ),
+            hours = Math.floor(timeRemaining / 60 / 60); 
+            return {timeRemaining, hours,  minutes, seconds};
+    }
+    
+      function updateClock (){
+        let timer = (getTimeRemaining());
+        // дописываем ноль в начало цифры
+        function newFormat (n){
+            if (n < 10){
+                return '0' + n;
+            }else {
+                return n;
+            }
+        }
 
+        if (timer.timeRemaining > 0){
+            timerHours.textContent = newFormat(timer.hours);
+            timerMinutes.textContent = newFormat(timer.minutes);
+            timerSeconds.textContent =newFormat(timer.seconds);
+        }
+       
+        if (timer.timeRemaining < 0){
+            clearInterval(updateClock, 1000);
+                  
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+        }
+      
+      };
+      setInterval(updateClock, 1000);
+}
 
-// checkbox
-let checkbox = document.querySelectorAll('.deposit-checkmark')[0];
-console.log(checkbox);
+ countTimer('01 december 2020');
 
-// возможных доходов
-
- let possib = document.querySelectorAll('.additional_income-item')[0];
- console.log(possib);
-
- let possible = document.querySelectorAll('.additional_income-item')[1];
- console.log(possible);
-
-// бюджет
-let budgetdayvalue = document.getElementsByClassName('budget_day-value')[0];
-console.log(budgetdayvalue);
-
-let expensesmonthvalue = document.getElementsByClassName('expenses_month-value')[0];
-console.log( expensesmonthvalue);
-
-let addition = document.getElementsByClassName('additional_income-value')[0];
-console.log( addition);
-
-let additional = document.getElementsByClassName('additional_expenses-value')[0];
-console.log( additional);
-
-let income = document.getElementsByClassName('income_period-value')[0];
-console.log( income);
-
-
-let target = document.getElementsByClassName('expenses_month-value')[0];
-console.log( target);
-
-// поля ввода слева
-// первое название
-let ferst = document.querySelector('[class="income-title"]');
-console.log( ferst);
-
-// строка сумма
-let sumferst = document.querySelector('.income-amount');
-console.log( sumferst);
-
- // второе  название
- let second = document.querySelector('input.income-title');
- console.log( second);
-
- // строка сумма 2
-
- let sumsecond = document.querySelector('.additional_income-item');
- console.log( sumsecond);
-
-
- // третье  название
- let third = document.querySelector('input.income-title');
- console.log( third);
-
- // строка сумма 3
- let sumthird = document.querySelector('input.additional_income-item');
- console.log( sumthird);
-
- //range 
- let range = document.querySelector('.period-select');
- console.log( range);
-
- // сумма 
- let summa = document.querySelector('input.target-amount');
- console.log( summa);
-
- // месячный доход сумма
-
- let total = document.querySelector('input.salary-amount');
- console.log( total);
-
-
- // обязательные расходы
- // название
- let fourth = document.querySelector('input.expenses-title');
- console.log(fourth);
-
- // сумма
- let cost = document.querySelector('input.expenses-amount');
- console.log(cost);
+});
